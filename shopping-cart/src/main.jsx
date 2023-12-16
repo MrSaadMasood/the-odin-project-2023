692063f1779613243ec4b6c3dd1c44d44fbba632
MrSaadMasood <144701669+MrSaadMasood@users.noreply.github.com>
2023-12-16 23:35:10 +0500

    commit in main wihtout testing

--- shopping-cart/src/main.jsx
+++ shopping-cart/src/main.jsx
@@ -26,9 +26,7 @@ const router = createBrowserRouter(
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
     <RouterProvider router={router}>
       <App />
     </RouterProvider>
-  // </React.StrictMode>,
 )
