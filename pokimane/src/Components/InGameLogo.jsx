60c9e07999ee7f21acaaacf27920c14a7e1f8a7a
MrSaadMasood <mrsaadmasood1@gmail.com>
2024-02-21 14:57:52 +0500

    feat: add tests

--- pokimane/src/Components/InGameLogo.jsx
+++ pokimane/src/Components/InGameLogo.jsx
@@ -1,6 +1,6 @@
 export default function InGameLogo({ resetGameFunction }) {
   return (
-    <button onClick={resetGameFunction}>
+    <button onClick={resetGameFunction} data-testid="button">
       <div className="in-game-logo flex justify-center items-center absolute top-4 left-3 w-44 sm:w-[15rem] md:w-[18rem] lg:w-[22rem] hover:scale-110 ease-in duration-100 ">
         <img src="../assets/pokimane.png" alt="" width={"500px"} />
       </div>
--- pokimane/src/tests/Card.test.jsx
+++ pokimane/src/tests/Card.test.jsx
@@ -2,7 +2,12 @@ import { it, vi, expect, assert } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Card from "../Components/Card";
 import userEvent from "@testing-library/user-event";
-
+import GameOver from "../Components/GameOver";
+import {toBeInTheDocument} from "@testing-library/jest-dom"
+import InGameLogo from "../Components/InGameLogo";
+import ScoreBoard from "../Components/ScoreBoard";
+import SelectMode from "../Components/SelectMode";
+import PlayArea from "../Components/PlayArea";
 it("renders the Card component with initial data", () => {
   const user = userEvent.setup()
   const mockData = {
@@ -31,6 +36,45 @@ it("renders the Card component with initial data", () => {
 
 
   assert.ok(screen.getByText("Pikachu"));
-//  expect(isCardClickedFunction).toHaveBeenCalled()
 });
 
+it("tests the Game ober component" ,async ()=>{
+  const result = vi.fn()
+  const topScore = 16;
+  const resetGameFunction = vi.fn()
+  render(<GameOver result={result} topScore={topScore} resetGameFunction={resetGameFunction} />)
+  const user = userEvent.setup()
+  const text = screen.getByText(/16/i)
+  const button = screen.getByRole("button", { name : "Play Again!"})
+  await user.click(button)
+  expect(text).toBeInTheDocument()
+  expect(resetGameFunction).toHaveBeenCalled()
+})
+
+it("tests the inGameLogo component", async ()=>{
+  const resetGameFunction = vi.fn()
+  render(<InGameLogo resetGameFunction={resetGameFunction} />)
+  const button = screen.getByTestId("button")
+  await userEvent.click(button)
+  expect(resetGameFunction).toHaveBeenCalled()
+})
+
+it("tests the scoreBoard Component", ()=>{
+  render(<ScoreBoard score={10} topScore={20} level={3} />)
+  expect(screen.getByText(/10/i)).toBeInTheDocument()
+  expect(screen.getByText(/20/i)).toBeInTheDocument()
+  expect(screen.getByText(/3/i)).toBeInTheDocument()
+})
+
+it("tests the handleMode component", async()=>{
+  const handleModeSelection = vi.fn()
+  render(<SelectMode handleModeSelection={handleModeSelection} />)
+  expect(screen.getByText(/memory/i)).toBeInTheDocument()
+  const button = screen.getByRole("button", { name : "Easy"})
+  const button2= screen.getByRole("button", { name : "Hard"})
+  await userEvent.click(button)
+  expect(handleModeSelection).toHaveBeenCalledTimes(1)
+
+  await userEvent.click(button2)
+  expect(handleModeSelection).toHaveBeenCalledTimes(2)
+})
